83fc80c1693bd105d205cff57ecf1e7b8e3901b3
stu-dent101 <katoya.ottley@my.uwi.edu>
2024-11-12 18:17:47 -0400

    Update admin.py
    Changed the admin query filter to find the first admin with the email that was provided with the login details rather than comparing the u_id with the email

--- App/controllers/admin.py
+++ App/controllers/admin.py
@@ -2,7 +2,7 @@ from App.models import Admin
 from App.database import db
 
 def login_admin(email, password):
-    admin = db.session.query(Admin).filter(Admin.u_ID==email).first()
+    admin = db.session.query(Admin).filter(Admin.email==email).first()
     if admin != None:
         if admin.check_password(password):
             return admin.login()
