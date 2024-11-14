309f35314569a34b989352cfee65a4a744f70a2e
stu-dent101 <katoya.ottley@my.uwi.edu>
2024-11-14 02:05:10 -0400

    Updated courseAssessment naming conventions
    Updated courseAssessment naming conventions to match the new model diagram

--- App/controllers/courseAssessment.py
+++ App/controllers/courseAssessment.py
@@ -3,11 +3,11 @@ from App.models import Assessment
 from App.models import Course
 from App.database import db
 
-def add_CourseAsm(courseCode, a_ID, startDate, endDate, startTime, endTime, clashDetected):
+def add_CourseAsm(courseCode, assessmentID, assessmentTitle, assessmentType, startDate, dueDate): #startTime, endTime, clashDetected were removed  
     #Add new Assessment to Course
     # newAsm = addCourseAsg(courseCode, a_ID, startDate, endDate, startTime, endTime)
     # return newAsm
-    newAsg = CourseAssessment(courseCode, a_ID, startDate, endDate, startTime, endTime, clashDetected)
+    newAsg = CourseAssessment(courseCode, assessmentID, assessmentTitle, assessmentType, startDate, dueDate) #startTime, endTime, clashDetected were removed  
     db.session.add(newAsg)  #add to db
     db.session.commit()
     return newAsg
@@ -17,13 +17,13 @@ def list_Assessments():
 
 def get_Assessment_id(aType):
     assessment=Assessment.query.filter_by(category=aType).first()
-    return assessment.a_ID
+    return assessment.assessmentID
 
-def get_Assessment_type(id):
-    assessment=Assessment.query.filter_by(a_ID=id).first()
+def get_Assessment_type(assessmentID):
+    assessment=Assessment.query.filter_by(assessmentID=assessmentID).first()
     return assessment.category.name
 
-def get_CourseAsm_id(id):
+def get_CourseAsm_id(id): #is the courseCode being used here? *New Comment
     return CourseAssessment.query.filter_by(id=id).first()   
 
 def get_CourseAsm_code(code):
@@ -36,10 +36,12 @@ def get_CourseAsm_level(level):
         assessments = assessments + get_CourseAsm_code(c)
     return assessments
 
-def delete_CourseAsm(courseAsm):
+def delete_CourseAsm(courseAsm): #delete based on their assessmentID e.g.delete_CourseAsm(assessmentID)
     db.session.delete(courseAsm)
     db.session.commit()
     return True        
-     
+
+
+#Will have to get back to this
 def get_clashes():
     return CourseAssessment.query.filter_by(clashDetected=True).all()
