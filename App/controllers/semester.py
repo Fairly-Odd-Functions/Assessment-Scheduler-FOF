403ff61398fcf6284437c0226a6d47324835317c
stu-dent101 <katoya.ottley@my.uwi.edu>
2024-11-14 02:03:37 -0400

    Updated semester naming conventions
    Updated semester naming conventions to match the new model diagram

--- App/controllers/semester.py
+++ App/controllers/semester.py
@@ -1,8 +1,8 @@
 from App.models import Semester
 from App.database import db
 
-def add_sem(startDate,endDate,semNum,maxAssessments):
-    new_sem = Semester(startDate=startDate,endDate=endDate,semNum=semNum,maxAssessments=maxAssessments)
+def add_sem(startDate,endDate,semesterID,semesterTitle): #maxAssessments removed
+    new_sem = Semester(startDate=startDate,endDate=endDate,semesterID=semesterID,semesterTitle=semesterTitle)
     db.session.add(new_sem)
     db.session.commit()
     return new_sem
