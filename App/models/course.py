bf8847ea7098364b7671da0403ad8a725735724a
DaNJO-Y <daniel.young@my.uwi.edu>
2024-11-21 15:00:02 +0000

    Added totalstudentsenrolled attribte to Course

--- App/models/course.py
+++ App/models/course.py
@@ -6,21 +6,22 @@ class Course(db.Model):
   courseCode = db.Column(db.String(9), primary_key=True)
   offeredSemester = db.Column(db.Integer,db.ForeignKey('semester.semesterID'), nullable=False)
   clashRules = db.relationship('ClashRule',backref='course', lazy=True)
-   
   clashRuleID = db.Column(db.Integer,db.ForeignKey('clashrule.clashRuleID'),nullable=False)
   courseTitle = db.Column(db.String(120), nullable=False)
   courseCredits = db.Column(db.Integer)
   courseDescription = db.Column(db.String(1024), nullable=False)
   courseLevel = db.Column(db.Integer, nullable=False)
-  
+  totalStudentsEnrolled = db.Column(db.Integer)
 
-  def __init__(self, courseCode, offeredSemester, courseTitle, courseCredits,  courseDescription, courseLevel):
+  def __init__(self, courseCode, offeredSemester, courseTitle, courseCredits,  courseDescription, courseLevel, totalStudentsEnrolled):
     self.courseCode = courseCode
     self.offeredSemester = offeredSemester
     self.courseTitle = courseTitle
     self.courseCredits = courseCredits
     self.courseDescription = courseDescription
     self.courseLevel = courseLevel
+    self.totalStudentsEnrolled = totalStudentsEnrolled
+
     
   def to_json(self):
     return {
@@ -29,6 +30,7 @@ class Course(db.Model):
       "courseTitle" : self.courseTitle,
       "courseCredits": self.courseCredits,
       "courseDescription" : self.courseDescription,
-      "courseLevel" : self.courseLevel
+      "courseLevel" : self.courseLevel,
+      "totalStudentsEnrolled" : self.totalStudentsEnrolled
      
     }
