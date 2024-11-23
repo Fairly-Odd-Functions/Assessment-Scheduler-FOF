025762f2144ecd4e0865055cdea44ef18e73530c
stu-dent101 <katoya.ottley@my.uwi.edu>
2024-11-23 13:49:34 -0400

    Updated Admin Controller

--- App/controllers/admin.py
+++ App/controllers/admin.py
@@ -2,21 +2,37 @@ from App.models import Admin, ClashRule
 from App.database import db
 
 def login_admin(email, password):
-    admin = db.session.query(Admin).filter(Admin.email==email).first()
-    if admin != None:
-        if admin.check_password(password):
-            return admin.login()
-    return "Login failed"
+    try:
+        if not email or not password:
+            return {"Error Message": "All fields are required"}
+        
+        admin = db.session.query(Admin).filter(Admin.email==email).first()
+        if admin != None:
+            if admin.check_password(password):
+                return {"Login Successful": admin.login()}
+        return {"Error Message" : "Invalid email or password"}
+    
+    except Exception as e:
+        print(f"Error Login Failed: {e}")
+        return None 
 
 def custom_clash_rule(clashRuleID, userID, clashRuleTitle, clashRuleDescription, allowableDays): #create_new_clash_rule? as a possible function name
-    rule = ClashRule.query.get(clashRuleID)
-    if rule: 
-        return rule
-    else:
-         #Add new rule
-        newRule = ClashRule.addNewRule(clashRuleID, userID, clashRuleTitle, clashRuleDescription, allowableDays) #possible name "addNewRule?" for the adding new rules/custome rules that would be in the models section
+    try:
+        if not clashRuleID or not userID or not clashRuleTitle or not clashRuleDescription or not allowableDays:
+            return {"Error Message": "All fields are required"}
+        
+        rule = ClashRule.query.get(clashRuleID)
+        if rule: 
+            return {"Rule Already Exists": rule}
+        
+        #Add new rule
+        newRule = ClashRule(clashRuleID = clashRuleID, userID = userID, clashRuleTitle = clashRuleTitle, clashRuleDescription  = clashRuleDescription, allowableDays = allowableDays)
         db.session.add(newRule)
         db.session.commit()
-        return newRule
+        return {"New Rule Added": newRule}
+    
+    except Exception as e:
+        print(f"Error Adding New Rule: {e}")
+        db.session.rollback() 
+        return None 
 
-    #Note the class and function call names and the ordering of the parameter might have to change depending on the models    
