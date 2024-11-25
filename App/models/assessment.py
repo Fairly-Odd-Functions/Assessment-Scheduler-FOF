29322707018a74c5334558e7f2bfb82a596beaef
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 16:03:03 -0400

    Task 05.9 Refactoring : Assessment Model
    Clean up on assesment.py included:
    - Remove category attribute
    - Add str()function
    - Add repr() function
    
    Clean up on courseAssesment.py included:
    - DELETETION of courseAssessment Model

--- App/models/assessment.py
+++ App/models/assessment.py
@@ -1,27 +1,45 @@
 from App.database import db
 import enum
 
-class Category(enum.Enum):
-    EXAM = "Exam"
-    ASSIGNMENT = "Assignment"
-    QUIZ = "Quiz"
-    PROJECT = "Project"
-    DEBATE = "Debate"
-    PRESENTATION = "Presentation"
-    ORALEXAM = "Oral Exam"
-    PARTICIPATION = "Participation"
-
 class Assessment(db.Model):
     __tablename__ = 'assessment'
 
-    a_ID = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    category = db.Column(db.Enum(Category), nullable=False)
+    assessmentID = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    courseCode = db.Column(db.Integer, db.ForeignKey('course.courseCode'),nullable=False)
+    assessmentTitle = db.Column(db.String(120),nullable=False)
+    assessmentType = db.Column(db.String(120),nullable=False)
+    startDate = db.Column(db.Date)
+    dueDate = db.Column(db.Date)
 
-    def __init__(self, category):
-        self.category = category
+    def __init__(self, assessmentID, courseCode, assessmentTitle, assessmentType, startDate, dueDate):
+            self.assessmentID = assessmentID
+            self.courseCode = courseCode
+            self.assessmentTitle = assessmentTitle
+            self.assessmentType = assessmentType
+            self.startDate = startDate
+            self.dueDate = dueDate    
 
-    def to_json(self):
+    def get_json(self):
         return {
-        "a_ID" : self.a_ID,
-        "category" : self.category
-        }     
+            'assessmentID': self.assessmentID,
+            'courseCode': self.courseCode,
+            'assessmentTitle': self.assessmentTitle,
+            'assessmentType': self.assessmentType,
+            'startDate': self.startDate,
+            'dueDate': self.dueDate
+        }  
+    
+    def __str__(self):
+        return f"assessmentID={self.assessmentID}, 
+                courseCode={self.courseCode}, 
+                assessmentTitle={self.assessmentTitle}, 
+                assessmentType={self.assessmentType}, 
+                startDate={self.startDate}, 
+                dueDate={self.dueDate}"
+
+    def __repr__(self):
+        return (f"<Assessment: {self.assessmentTitle} | "
+                f"Course Code: {self.courseCode} | "
+                f"Type: {self.assessmentType} | "
+                f"Start Date: {self.startDate} | "
+                f"Due Date: {self.dueDate}>")
