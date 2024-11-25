44061e6e18c8c224b4bea6106bd307b0248d2fe9
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 12:29:12 -0400

    Clean up on CourseProgramme Model
    Clean up on coursProgramme.oy included:
    
    - Rename to_json() to get_json()
    - Edit get—json()
    - Add str function
    - Add repr function

--- App/models/courseProgramme.py
+++ App/models/courseProgramme.py
@@ -7,15 +7,21 @@ class CourseProgramme(db.Model):
   programmeID = db.Column(db.Integer, db.ForeignKey('programme.programmeID'), nullable = False)   
   courseCode = db.Column(db.String(8), db.ForeignKey('course.courseCode'), nullable = False)
  
-
   def __init__(self, programmeID, courseCode):
     self.programmeID = programmeID
     self.courseCode = courseCode
     
-    
-  def to_json(self):
+  def get_json(self):
     return {
-      "programmeID": self.programmeID,
-      "courseCode": self.courseCode
-      
-    }  
+        'courseProgrammeID': self.courseProgrammeID,
+        'programmeID': self.programmeID,
+        'courseCode': self.courseCode
+    } 
+  
+  def __str__(self):
+    return f" programmeID={self.programmeID}, 
+              courseCode={self.courseCode})"
+  
+  def __repr__(self):
+    return (f"<programmeID: {self.programmeID} | "
+            f"courseCode: {self.courseCode}>")
