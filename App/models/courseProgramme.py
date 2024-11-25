1f06af5b36604fc754e22cbf1186604e5caf1d05
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 14:53:02 -0400

    Revert "Clean up on CourseProgramme Model"
    This reverts commit 44061e6e18c8c224b4bea6106bd307b0248d2fe9.

--- App/models/courseProgramme.py
+++ App/models/courseProgramme.py
@@ -7,21 +7,15 @@ class CourseProgramme(db.Model):
   programmeID = db.Column(db.Integer, db.ForeignKey('programme.programmeID'), nullable = False)   
   courseCode = db.Column(db.String(8), db.ForeignKey('course.courseCode'), nullable = False)
  
+
   def __init__(self, programmeID, courseCode):
     self.programmeID = programmeID
     self.courseCode = courseCode
     
-  def get_json(self):
+    
+  def to_json(self):
     return {
-        'courseProgrammeID': self.courseProgrammeID,
-        'programmeID': self.programmeID,
-        'courseCode': self.courseCode
-    } 
-  
-  def __str__(self):
-    return f" programmeID={self.programmeID}, 
-              courseCode={self.courseCode})"
-  
-  def __repr__(self):
-    return (f"<programmeID: {self.programmeID} | "
-            f"courseCode: {self.courseCode}>")
+      "programmeID": self.programmeID,
+      "courseCode": self.courseCode
+      
+    }  
