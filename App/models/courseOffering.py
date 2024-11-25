fd36235fc4864c078e06283bef5e9b10c2c07aa6
Jalene Is'raell Armstrong <jalene.armstrong@my.uwi.edu>
2024-11-25 16:35:18 -0400

    Task 05.4 Refactoring : Course Model
    The course model is supposed to the basic "model"/template in creating a course, those the bare minimum of information is used to make it.
    
    A courseOffering model is introduced to handle more dynamic occurrences that relates to a specific course, i.e. The academic year the course is being offered and the amount of students enrolled into the course for that specific academic year and semester
    
    1. Removed semester and aNum attributes
    2. Introduced courseOffering model, to dynamically handle totalStudentsEnrolled and the academic year of the course.
    3. Adjusted init and get_json functions
    4. Introduced str and repr

---
+++ App/models/courseOffering.py
@@ -0,0 +1,35 @@
+from App.database import db
+
+class CourseOffering(db.Model):
+    __tablename__ = 'course_offering'
+
+    # Attributes
+    offeringID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    courseCode = db.Column(db.String(9), db.ForeignKey('course.courseCode'), nullable=False)
+    semesterID = db.Column(db.Integer, db.ForeignKey('semester.semesterID'), nullable=False)
+    academicYear = db.Column(db.String(9), nullable=False)
+    totalStudentsEnrolled = db.Column(db.Integer, default=0)
+
+    def __init__(self, courseCode, semesterID, academicYear, totalStudentsEnrolled=0):
+        self.courseCode = courseCode
+        self.semesterID = semesterID
+        self.academicYear = academicYear
+        self.totalStudentsEnrolled = totalStudentsEnrolled
+
+    def get_json(self):
+        return {
+            "offeringID": self.offeringID,
+            "courseCode": self.courseCode,
+            "semesterID": self.semesterID,
+            "academicYear": self.academicYear,
+            "totalStudentsEnrolled": self.totalStudentsEnrolled,
+        }
+
+    def __str__(self):
+        return f"{self.course.courseTitle} ({self.courseCode}) - {self.academicYear}, Semester {self.semesterID}"
+
+    def __repr__(self):
+        return (
+            f"CourseOffering(courseCode='{self.courseCode}', academicYear='{self.academicYear}', "
+            f"semesterID={self.semesterID}, totalStudentsEnrolled={self.totalStudentsEnrolled})"
+        )
