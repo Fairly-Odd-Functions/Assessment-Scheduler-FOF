59564e2ec4ef15dfa005ad61e9fb8c95ea416c1e
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 11:45:58 -0400

    Cleaned up Admin Model
    Clean up admin.py involved the following:
    
    -Remove staffType
    -Remove unneeded code/commented out
     code
    - Include get_json()
    - Remove login()
    - Remove duplicated imports
    - Fix init to reference adminlD rather than userlD
    - Remove createStaff controller

--- App/models/admin.py
+++ App/models/admin.py
@@ -1,11 +1,5 @@
 from App.database import db
-from .user import User 
 from App.models import User 
-from flask_login import UserMixin, login_user
-import flask_login
-from flask_login import UserMixin, login_user
-import flask_login
-
 
 class Admin(User):
   __tablename__ = 'admin'
@@ -14,18 +8,17 @@ class Admin(User):
   __mapper_args__ ={
     'polymorphic_identity': 'admin'
   }
-
-  def login(self):
-      return flask_login.login_user(self)
   
-  def __init__(self, userID, firstName, lastName, password, email):
-    super().__init__(userID, firstName, lastName, password, email)
-
-  def __createStaff__(userID, firstName, lastName, password, email, status):
-    newStaff = Staff(userID, firstName, lastName, password, email, status)
-    db.session.add(newStaff)  #add to db
-    db.session.commit()
-    return newStaff
+  def __init__(self, adminID, firstName, lastName, password, email):
+    # Calling the constructor of the parent class (User) with the provided arguments
+    super().__init__(adminID, firstName, lastName, password, email)
 
   def __str__(self):
-        return f"Admin(id={self.adminID}, email={self.email})" 
+    return f"Admin(id={self.adminID}, email={self.email})" 
+  
+  def get_json(self):
+    return {
+      "firstName": self.firstName,
+      "lastName": self.lastName,
+      "email": self.email
+    }  
