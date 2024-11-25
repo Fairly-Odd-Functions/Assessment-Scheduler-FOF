9ed9871db882c8d0b67be60a4e68f37b094ee7b8
Jalene Is'raell Armstrong <jalene.armstrong@my.uwi.edu>
2024-11-25 17:54:13 -0400

    Task 05.8 Refactoring : Semester Model
    1. Include str and repr
    2. Included academicYear here instead, to be removed from courseOffering Model in other branch
    3. Adjusted get_json and init to suit

--- App/models/semester.py
+++ App/models/semester.py
@@ -2,23 +2,33 @@ from App.database import db
 
 class Semester(db.Model):
     __tablename__='semester'
-    id = db.Column(db.Integer, primary_key= True, autoincrement=True)
+
+    # Attributes
+    semesterID = db.Column(db.Integer, primary_key= True, autoincrement=True)
+    semesterName = db.Column(db.String(120),nullable = False)
+    academicYear = db.Column(db.String(9), nullable=False)
     startDate = db.Column(db.Date,nullable=False)
     endDate = db.Column(db.Date,nullable=False)
-    semNum = db.Column(db.Integer,nullable=False)
-    maxAssessments = db.Column(db.Integer,nullable=False)
 
-def __init__(self, startDate, endDate, semNum, maxAssessments):
-    self.startDate = startDate
-    self.endDate = endDate
-    self.semNum = semNum
-    self.maxAssessments = maxAssessments
+    def __init__(self, semesterName, academicYear, startDate, endDate):
+        self.semesterName = semesterName
+        self.academicYear = academicYear
+        self.startDate = startDate
+        self.endDate = endDate
 
-def to_json(self):
-    return{
-        "id":self.id,
-        "startDate":self.startDate,
-        "endDate":self.endDate,
-        "semNum":self.semNum,
-        "maxAssessments":self.maxAssessments
-    }
+    def get_json(self):
+        return{
+            "semester_name":self.semesterName,
+            "academicYear": self.academicYear,
+            "startDate":self.startDate,
+            "endDate":self.endDate
+        }
+
+    def __str__(self):
+        return f"{self.semesterName} {self.academicYear} (Start: {self.startDate}, End: {self.endDate})"
+
+    def __repr__(self):
+        return (
+            f"Semester(semesterID={self.semesterID}, semesterName='{self.semesterName}', "
+            f"academicYear='{self.academicYear}', startDate='{self.startDate}', endDate='{self.endDate}')"
+        )
