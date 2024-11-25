e863f74ad9cfa11d5e330f21400b8fa1b66c6079
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 12:46:28 -0400

    Cleaned up Programme Model
    Clean up on programme.py included:
    
    -Rename to_json() to get_json()
    - Re-Add relationship attribute programmeCourses
    - Add _str_ function
    - Add function

--- App/models/programme.py
+++ App/models/programme.py
@@ -6,14 +6,29 @@ class Programme(db.Model):
   programmeID = db.Column(db.Integer, primary_key = True, nullable=False, autoincrement=True)
   programmeTitle = db.Column(db.String(100), nullable = False)
   programmeDescription = db.Column(db.String(200), nullable = False)
+
+  # Relationship with the CourseProgramme model
+  programmeCourses = db.relationship('courseProgramme', backref='programme', lazy=True)
  
   def __init__(self, programmeTitle, programmeDescription):
     self.programmeTitle = programmeTitle
     self.programmeDescription = programmeDescription
 
-  def to_json(self):
-    return {
-    "programmeID" : self.programmeID,
-    "programmeTitle" : self.programmeTitle,
-    "programmeDescription" : self.programmeDescription
-    } 
+  def get_json(self):
+      return {
+          'programmeID': self.programmeID,
+          'programmeTitle': self.programmeTitle,
+          'programmeDescription': self.programmeDescription,
+          'programmeCourses': [course.get_json() for course in self.programmeCourses]
+      }
+  
+  def __str__(self):
+    return f"programmeID={self.programmeID}, 
+             programmeTitle={self.programmeTitle}, 
+             programmeDescription={self.programmeDescription},
+             programmeCourses={', '.join([course.courseCode for course in self.programmeCourses])})"
+
+  def __repr__(self):
+      return (f"<Programme: {self.programmeTitle} | "
+              f"Description: {self.programmeDescription} | "
+              f"Courses: {', '.join([course.courseCode for course in self.programmeCourses])}>")
