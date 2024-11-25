7efbe616039f1a10e440b9f8bd3ffd7a1616d998
Jalene Is'raell Armstrong <jalene.armstrong@my.uwi.edu>
2024-11-25 14:54:51 -0400

    Task 05.2 Refactoring : Staff Model
    As outlined in Task 05.10. Model Implementation Cleaning, several edits and updates were made.

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,75 +1,24 @@
-import flask_login
 from App.database import db
 from .user import User
-import enum
-from flask_login import UserMixin
 
-class Status(enum.Enum):
-    PTINSTRUCT = "Part-Time Instructor"
-    INSTRUCTOR = "Instructor"
-    HOD = "Head of Department"
-    LECTURER = "Lecturer"
-    TA = "Teaching Assisstant"
-    TUTOR = "Tutor"
-    PTTUTOR = "Part-Time Tutor"
-
-class Staff(User,UserMixin):
+class Staff(User):
   __tablename__ = 'staff'
-  fName = db.Column(db.String(120), nullable=False)
-  lName = db.Column(db.String(120), nullable=False)
-  cNum = db.Column(db.Integer, nullable=False, default=0) #changes depending on status
-  status = db.Column(db.Enum(Status), nullable = False) #defines the contract position of a teaching staff member
-  #creates reverse relationship from Staff back to Course to access courses assigned to a specific lecturer
-  coursesAssigned = db.relationship('CourseStaff', backref='courses', lazy='joined')
+  staffID = db.Column(db.Integer, db.ForeignKey('user.userID'), primary_key=True)
 
-  def __init__(self, fName, lName, u_ID, status, email, password):
-    super().__init__(u_ID, password, email)
-    self.fName = fName
-    self.lName = lName
-    if status == "Lecturer 1" or  "Lecturer 2" or  "Lecturer 3": #assign number of courses to staff depending on status
-      self.status = Status.LECTURER 
-      self.cNum = 2
-    else: 
-      self.cNum = 3 #Instructor
+  __mapper_args__ ={
+    'polymorphic_identity': 'staff'
+  }
 
-    # Other teaching positions for possible extension
-    # if status == "Part-Time Instructor": 
-    #   self.cNum = 1
-    # elif status == "Instructor": 
-    #   self.cNum = 2
-    # elif status == "Head of Department": 
-    #   self.cNum = 2  
-    # elif status == "Lecturer": 
-    #   self.cNum = 3
-    # elif status == "Teaching Assisstant": 
-    #   self.cNum = 2
-    # elif status == "Tutor": 
-    #   self.cNum = 2
-    # else: 
-    #   self.cNum = 1  #Part-Time Tutor
-    
-    
-  def get_id(self):
-    return self.u_ID 
+  def __init__(self, firstName, lastName, password, email):
+    # Passing Relevant Fields To User Constructor
+    super().__init__(firstName, lastName, password, email)
 
-  def to_json(self):
+  def get_json(self):
     return {
-        "staff_ID": self.u_ID,
-        "firstname": self.fName,
-        "lastname": self.lName,
-        "status": self.status,
-        "email": self.email,
-        "coursesNum": self.cNum,
-        "coursesAssigned": [course.to_json() for course in self.coursesAssigned]
-    }
+        "irst_name": self.firstName,
+        "last_name": self.lastName,
+        "email": self.email
+    } 
 
-  #Lecturers must register before using system
-  def register(firstName, lastName, u_ID, status, email, password):
-    newStaff = Staff(firstName, lastName, u_ID, status, email, password)
-    db.session.add(newStaff)  #add to db
-    db.session.commit()
-    return newStaff  
-  
-  def login(self):
-    return flask_login.login_user(self)
-
+  def __str__(self):
+        return f"Staff(ID={self.staffID}, email={self.email})"
