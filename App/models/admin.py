bf5e668e83d384b28be3e1fef7323c362e5c1bea
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 19:44:32 -0400

    Task 05.1 Refactoring : Admin Model
    Made changed according to review by @JaleneA

--- App/models/admin.py
+++ App/models/admin.py
@@ -3,22 +3,30 @@ from App.models import User
 
 class Admin(User):
   __tablename__ = 'admin'
+
+  #Attributes
   adminID = db.Column(db.Integer, db.ForeignKey('user.userID'), primary_key=True)
 
   __mapper_args__ ={
     'polymorphic_identity': 'admin'
   }
-  
-  def __init__(self, adminID, firstName, lastName, password, email):
-    # Calling the constructor of the parent class (User) with the provided arguments
-    super().__init__(adminID, firstName, lastName, password, email)
 
-  def __str__(self):
-    return f"Admin(id={self.adminID}, email={self.email})" 
-  
+  def __init__(self, firstName, lastName, password, email):
+    # Calling the constructor of the parent class (User) with the provided arguments
+    super().__init__(firstName, lastName, password, email)
+
   def get_json(self):
     return {
       "firstName": self.firstName,
       "lastName": self.lastName,
       "email": self.email
     }  
+
+  def __str__(self):
+    return f"Admin(firstname={self.adminID}, email={self.email})"
+
+  def __repr__(self):
+    return (
+      f"Admin(adminID={self.adminID}, firstName='{self.firstName}', lastName='{self.lastName}', "
+      f"email='{self.email}')"
+    )
