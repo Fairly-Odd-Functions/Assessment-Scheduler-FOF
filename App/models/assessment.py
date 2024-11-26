6b5422010fcbc5102d3cfd026d7c7dbca8b35fe0
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 22:05:46 -0400

    Task 05.9 Refactoring : Assessment Model
    Edited to suit updated review

--- App/models/assessment.py
+++ App/models/assessment.py
@@ -1,34 +1,28 @@
 from App.database import db
-import enum
 
 class Assessment(db.Model):
     __tablename__ = 'assessment'
 
     #Attributes
     assessmentID = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    courseCode = db.Column(db.Integer, db.ForeignKey('course.courseCode'),nullable=False)
     assessmentTitle = db.Column(db.String(120),nullable=False)
     assessmentType = db.Column(db.String(120),nullable=False)
 
-    def __init__(self, courseCode, assessmentTitle, assessmentType):
-            self.courseCode = courseCode
+    def __init__(self, assessmentTitle, assessmentType):
             self.assessmentTitle = assessmentTitle
             self.assessmentType = assessmentType 
 
     def get_json(self):
         return {
             'assessmentID': self.assessmentID,
-            'courseCode': self.courseCode,
             'assessmentTitle': self.assessmentTitle,
             'assessmentType': self.assessmentType,
         }  
     
     def __str__(self):
-        return f"courseCode={self.courseCode}, 
-                assessmentTitle={self.assessmentTitle},
+        return f"assessmentTitle={self.assessmentTitle},
                 assessmentType={self.assessmentType}"
 
     def __repr__(self):
         return (f"<Assessment(assessmentTitle='{self.assessmentTitle}', "
-                f"courseCode='{self.courseCode}', "
                 f"assessmentType='{self.assessmentType}')>")
