28bd3d873e8dc24c4f9aa14fe2c5bbe5455a3b50
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 21:52:54 -0400

    Task 05.9 Refactoring : Assessment Model
    Edited changes to suit review made on pull request

--- App/models/assessment.py
+++ App/models/assessment.py
@@ -4,20 +4,16 @@ import enum
 class Assessment(db.Model):
     __tablename__ = 'assessment'
 
+    #Attributes
     assessmentID = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     courseCode = db.Column(db.Integer, db.ForeignKey('course.courseCode'),nullable=False)
     assessmentTitle = db.Column(db.String(120),nullable=False)
     assessmentType = db.Column(db.String(120),nullable=False)
-    startDate = db.Column(db.Date)
-    dueDate = db.Column(db.Date)
 
-    def __init__(self, assessmentID, courseCode, assessmentTitle, assessmentType, startDate, dueDate):
-            self.assessmentID = assessmentID
+    def __init__(self, courseCode, assessmentTitle, assessmentType):
             self.courseCode = courseCode
             self.assessmentTitle = assessmentTitle
-            self.assessmentType = assessmentType
-            self.startDate = startDate
-            self.dueDate = dueDate    
+            self.assessmentType = assessmentType 
 
     def get_json(self):
         return {
@@ -25,21 +21,14 @@ class Assessment(db.Model):
             'courseCode': self.courseCode,
             'assessmentTitle': self.assessmentTitle,
             'assessmentType': self.assessmentType,
-            'startDate': self.startDate,
-            'dueDate': self.dueDate
         }  
     
     def __str__(self):
-        return f"assessmentID={self.assessmentID}, 
-                courseCode={self.courseCode}, 
-                assessmentTitle={self.assessmentTitle}, 
-                assessmentType={self.assessmentType}, 
-                startDate={self.startDate}, 
-                dueDate={self.dueDate}"
+        return f"courseCode={self.courseCode}, 
+                assessmentTitle={self.assessmentTitle},
+                assessmentType={self.assessmentType}"
 
     def __repr__(self):
-        return (f"<Assessment: {self.assessmentTitle} | "
-                f"Course Code: {self.courseCode} | "
-                f"Type: {self.assessmentType} | "
-                f"Start Date: {self.startDate} | "
-                f"Due Date: {self.dueDate}>")
+        return (f"<Assessment(assessmentTitle='{self.assessmentTitle}', "
+                f"courseCode='{self.courseCode}', "
+                f"assessmentType='{self.assessmentType}')>")
