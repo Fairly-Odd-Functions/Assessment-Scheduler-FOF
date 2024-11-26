b1340c27695931b204a3b2803b1aba057b22e032
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-25 21:10:33 -0400

    Task 05.7 Recatoring : Programme Model
    Made adjustments based on review

--- App/models/programme.py
+++ App/models/programme.py
@@ -3,32 +3,27 @@ from App.database import db
 class Programme(db.Model):
   __tablename__ = 'programme'
 
+  #Attributes
   programmeID = db.Column(db.Integer, primary_key = True, nullable=False, autoincrement=True)
   programmeTitle = db.Column(db.String(100), nullable = False)
   programmeDescription = db.Column(db.String(200), nullable = False)
-
-  # Relationship with the CourseProgramme model
-  programmeCourses = db.relationship('courseProgramme', backref='programme', lazy=True)
  
   def __init__(self, programmeTitle, programmeDescription):
     self.programmeTitle = programmeTitle
     self.programmeDescription = programmeDescription
 
   def get_json(self):
-      return {
-          'programmeID': self.programmeID,
-          'programmeTitle': self.programmeTitle,
-          'programmeDescription': self.programmeDescription,
-          'programmeCourses': [course.get_json() for course in self.programmeCourses]
-      }
+    return {
+        'programmeID': self.programmeID,
+        'programmeTitle': self.programmeTitle,
+        'programmeDescription': self.programmeDescription,
+    }
   
   def __str__(self):
-    return f"programmeID={self.programmeID}, 
-             programmeTitle={self.programmeTitle}, 
-             programmeDescription={self.programmeDescription},
-             programmeCourses={', '.join([course.courseCode for course in self.programmeCourses])})"
+    return (f"programmeID={self.programmeID}, "
+            f"programmeTitle={self.programmeTitle}, "
+            f"programmeDescription={self.programmeDescription[:30]}")
 
   def __repr__(self):
-      return (f"<Programme: {self.programmeTitle} | "
-              f"Description: {self.programmeDescription} | "
-              f"Courses: {', '.join([course.courseCode for course in self.programmeCourses])}>")
+    return (f"<Programme: {self.programmeTitle} | "
+            f"Description: {self.programmeDescription[:30]}>")
