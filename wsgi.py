d84582c4cad709f43dd936da9676fc082ba2ffc7
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-26 21:37:51 -0400

    Added 'add_staff' command

--- wsgi.py
+++ wsgi.py
@@ -2,16 +2,18 @@ import click, sys, csv
 from flask import Flask
 from App.main import create_app
 from App.database import db, get_migrate
-from App.main import create_app
-from App.database import db, get_migrate
 from flask.cli import with_appcontext, AppGroup
 from App.models import Staff, Course, Assessment, Programme, Admin
 from App.controllers import (
-    initialize
-from App.controllers import (
-    initialize
+    initialize,
+    register_staff,
+    update_staff,
+    get_staff_by_email
 )
- 
+
+#importing all controllers
+from App.controllers import *
+
 app = create_app()
 
 # This command creates and initializes the database
@@ -20,11 +22,28 @@ def init():
     initialize()
     print('\n Database Intialized! \n')
 
+
 '''
 |   Staff Group Commands
 |   These are a list of commands used to perform operations involving existing staff
 '''
 staff_cli = AppGroup('staff', help='Staff object commands')
+
+# COMMAND #1 - ADD STAFF
+@staff_cli.command('add', help='Add a new staff member')
+def add_staff():
+    
+    staffID = input("Enter Staff ID: ")
+    firstName = input("Enter First Name: ")
+    lastName = input("Enter Last Name: ")
+    email = input("Enter Email: ")
+
+    newstaff = register_staff(staffID, firstName, lastName, email)
+
+    if newstaff:
+        print(f"\n New Staff Member '{newstaff.firstName} {newstaff.lastName}'Added! \n")
+    else:
+        print("\n Staff Member Already Exists! \n")
 
 
 """
@@ -37,10 +56,6 @@ def get_users():
     for s in staff:
       print(s.to_json())
     print('end of staff objects')
-    staff = Staff.query.all()
-    for s in staff:
-      print(s.to_json())
-    print('end of staff objects')
 
 # This command creates all the Assessment objects
 @app.cli.command("asm")
@@ -49,14 +64,7 @@ def load_Asm():
     asm1 = Assessment(category='EXAM')
     db.session.add(asm1)
     db.session.commit()
-    db.create_all()
-    asm1 = Assessment(category='EXAM')
-    db.session.add(asm1)
-    db.session.commit()
 
-    asm2 = Assessment(category='ASSIGNMENT')
-    db.session.add(asm2)
-    db.session.commit()
     asm2 = Assessment(category='ASSIGNMENT')
     db.session.add(asm2)
     db.session.commit()
@@ -64,13 +72,7 @@ def load_Asm():
     asm3 = Assessment(category='QUIZ')
     db.session.add(asm3)
     db.session.commit()
-    asm3 = Assessment(category='QUIZ')
-    db.session.add(asm3)
-    db.session.commit()
 
-    asm4 = Assessment(category='PROJECT')
-    db.session.add(asm4)
-    db.session.commit()
     asm4 = Assessment(category='PROJECT')
     db.session.add(asm4)
     db.session.commit()
@@ -78,13 +80,7 @@ def load_Asm():
     asm5 = Assessment(category='DEBATE')
     db.session.add(asm5)
     db.session.commit()
-    asm5 = Assessment(category='DEBATE')
-    db.session.add(asm5)
-    db.session.commit()
 
-    asm6 = Assessment(category='PRESENTATION')
-    db.session.add(asm6)
-    db.session.commit()
     asm6 = Assessment(category='PRESENTATION')
     db.session.add(asm6)
     db.session.commit()
@@ -92,14 +88,7 @@ def load_Asm():
     asm7 = Assessment(category='ORALEXAM')
     db.session.add(asm7)
     db.session.commit()
-    asm7 = Assessment(category='ORALEXAM')
-    db.session.add(asm7)
-    db.session.commit()
 
-    asm8 = Assessment(category='PARTICIPATION')
-    db.session.add(asm8)
-    db.session.commit()
-    print('All assessments added')
     asm8 = Assessment(category='PARTICIPATION')
     db.session.add(asm8)
     db.session.commit()
@@ -112,14 +101,7 @@ def load_Pgr():
     pgr1 = Programme(p_name='Computer Science Major')
     db.session.add(pgr1)
     db.session.commit()
-    db.create_all()
-    pgr1 = Programme(p_name='Computer Science Major')
-    db.session.add(pgr1)
-    db.session.commit()
 
-    pgr2 = Programme(p_name='Computer Science Minor')
-    db.session.add(pgr2)
-    db.session.commit()
     pgr2 = Programme(p_name='Computer Science Minor')
     db.session.add(pgr2)
     db.session.commit()
@@ -127,13 +109,7 @@ def load_Pgr():
     pgr3 = Programme(p_name='Computer Science Special')
     db.session.add(pgr3)
     db.session.commit()
-    pgr3 = Programme(p_name='Computer Science Special')
-    db.session.add(pgr3)
-    db.session.commit()
 
-    pgr4 = Programme(p_name='Information Technology Major')
-    db.session.add(pgr4)
-    db.session.commit()
     pgr4 = Programme(p_name='Information Technology Major')
     db.session.add(pgr4)
     db.session.commit()
@@ -141,18 +117,11 @@ def load_Pgr():
     pgr5 = Programme(p_name='Information Technology Minor')
     db.session.add(pgr5)
     db.session.commit()
-    pgr5 = Programme(p_name='Information Technology Minor')
-    db.session.add(pgr5)
-    db.session.commit()
 
     pgr6 = Programme(p_name='Information Technology Special')
     db.session.add(pgr6)
     db.session.commit()
-    pgr6 = Programme(p_name='Information Technology Special')
-    db.session.add(pgr6)
-    db.session.commit()
 
-    print('All programmes added')
     print('All programmes added')
 
 # This command assigns courses to staff
@@ -170,30 +139,10 @@ def assign_course(staff_ID):
     db.session.commit()
     print(bob)
     print('courses added')
-    bob = Staff.query.filter_by(u_ID=staff_ID).first()
-
-    if not bob:
-        print(f'Staff with ID: {staff_ID} not found!')
-        return
-      
-    bob.coursesAssigned = ["COMP1601", "COMP1602", "COMP1603"]
-    db.session.add(bob)
-    db.session.commit()
-    print(bob)
-    print('courses added')
 
 #load course data from csv file
 @app.cli.command("load-courses")
 def load_course_data():
-    with open('courses.csv') as file: #csv files are used for spreadsheets
-      reader = csv.DictReader(file)
-      for row in reader: 
-        new_course = Course(courseCode=row['courseCode'], courseTitle=row['courseTitle'], description=row['description'], 
-          level=row['level'], semester=row['semester'], preReqs=row['preReqs'], p_ID=row['p_ID'],)  #create object
-        db.session.add(new_course) 
-      db.session.commit() #save all changes OUTSIDE the loop
-    print('database intialized')
-"""
     with open('courses.csv') as file: #csv files are used for spreadsheets
       reader = csv.DictReader(file)
       for row in reader: 
