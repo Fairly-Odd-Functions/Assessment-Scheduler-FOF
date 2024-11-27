c8fb3f3127b3dcbee658007563a9504d9d49fa94
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-27 11:58:53 -0400

    Completed 'add_staff', 'update_staff_profile' & 'delete_staff_profile'
    Forgot to exclude binary files.

--- wsgi.py
+++ wsgi.py
@@ -42,18 +42,18 @@ def add_staff(firstname, lastname, email, password):
 
 # COMMAND #2 - UPDATE STAFF
 @staff_cli.command('update', help='Update a staff member')
-@click.option('--staffemail', prompt='Enter the staff email', required=True)
-def update_staff(staffemail):
+@click.option('--staffemail', prompt='Enter Staff Email to Edit', required=True)
+def update_staff_profile(staffemail):
     
     staff_member = Staff.query.filter_by(email=staffemail).first()
     if not staff_member:
         print("\nStaff member not found!\n")
         return
     
-    firstname = click.prompt('Enter the new first name', default=staff_member.firstName)
-    lastname = click.prompt('Enter the new last name', default=staff_member.lastName)
-    email = click.prompt('Enter the new email', default=staff_member.email)
-    password = click.prompt('Enter the new password', confirmation_prompt=True, default='')
+    firstname = input('\nEdit First Name (press Enter to keep the current value): ') or None
+    lastname = input('Edit Last Name (press Enter to keep the current value): ') or None
+    email = input('Edit Email (press Enter to keep the current value): ') or None
+    password = input('Edit password (press Enter to keep the current value): ') or None
 
     updatedStaff = update_staff(staffemail, firstName=firstname, lastName=lastname, password=password, email=email)
     
@@ -64,190 +64,12 @@ def update_staff(staffemail):
 
 # COMMAND #3 - DELETE STAFF
 @staff_cli.command('delete', help='Delete a staff member')
-@click.option('--staff_email', prompt='Enter the staff email', required=True)
-def delete_staff(staff_email):
+@click.option('--staffemail', prompt='Enter the staff email', required=True)
+def delete_staff_profile(staffemail):
 
-    #Deleting staff
-    result = delete_staff(staff_email)
-    print(result)
+    result = delete_staff(staffemail)
+    print("\n" + result)
 
     
 
 app.cli.add_command(staff_cli)
-
-"""
-TO BE REFACTORED ~ JaleneA
-
-# This command retrieves all staff objects
-@app.cli.command('get-users')
-def get_users():
-    staff = Staff.query.all()
-    for s in staff:
-      print(s.to_json())
-    print('end of staff objects')
-    staff = Staff.query.all()
-    for s in staff:
-      print(s.to_json())
-    print('end of staff objects')
-
-# This command creates all the Assessment objects
-@app.cli.command("asm")
-def load_Asm():
-    db.create_all()
-    asm1 = Assessment(category='EXAM')
-    db.session.add(asm1)
-    db.session.commit()
-    db.create_all()
-    asm1 = Assessment(category='EXAM')
-    db.session.add(asm1)
-    db.session.commit()
-
-    asm2 = Assessment(category='ASSIGNMENT')
-    db.session.add(asm2)
-    db.session.commit()
-    asm2 = Assessment(category='ASSIGNMENT')
-    db.session.add(asm2)
-    db.session.commit()
-
-    asm3 = Assessment(category='QUIZ')
-    db.session.add(asm3)
-    db.session.commit()
-    asm3 = Assessment(category='QUIZ')
-    db.session.add(asm3)
-    db.session.commit()
-
-    asm4 = Assessment(category='PROJECT')
-    db.session.add(asm4)
-    db.session.commit()
-    asm4 = Assessment(category='PROJECT')
-    db.session.add(asm4)
-    db.session.commit()
-
-    asm5 = Assessment(category='DEBATE')
-    db.session.add(asm5)
-    db.session.commit()
-    asm5 = Assessment(category='DEBATE')
-    db.session.add(asm5)
-    db.session.commit()
-
-    asm6 = Assessment(category='PRESENTATION')
-    db.session.add(asm6)
-    db.session.commit()
-    asm6 = Assessment(category='PRESENTATION')
-    db.session.add(asm6)
-    db.session.commit()
-
-    asm7 = Assessment(category='ORALEXAM')
-    db.session.add(asm7)
-    db.session.commit()
-    asm7 = Assessment(category='ORALEXAM')
-    db.session.add(asm7)
-    db.session.commit()
-
-    asm8 = Assessment(category='PARTICIPATION')
-    db.session.add(asm8)
-    db.session.commit()
-    print('All assessments added')
-    asm8 = Assessment(category='PARTICIPATION')
-    db.session.add(asm8)
-    db.session.commit()
-    print('All assessments added')
-
-# This command creates all the Programme objects
-@app.cli.command("pgr")
-def load_Pgr():
-    db.create_all()
-    pgr1 = Programme(p_name='Computer Science Major')
-    db.session.add(pgr1)
-    db.session.commit()
-    db.create_all()
-    pgr1 = Programme(p_name='Computer Science Major')
-    db.session.add(pgr1)
-    db.session.commit()
-
-    pgr2 = Programme(p_name='Computer Science Minor')
-    db.session.add(pgr2)
-    db.session.commit()
-    pgr2 = Programme(p_name='Computer Science Minor')
-    db.session.add(pgr2)
-    db.session.commit()
-
-    pgr3 = Programme(p_name='Computer Science Special')
-    db.session.add(pgr3)
-    db.session.commit()
-    pgr3 = Programme(p_name='Computer Science Special')
-    db.session.add(pgr3)
-    db.session.commit()
-
-    pgr4 = Programme(p_name='Information Technology Major')
-    db.session.add(pgr4)
-    db.session.commit()
-    pgr4 = Programme(p_name='Information Technology Major')
-    db.session.add(pgr4)
-    db.session.commit()
-
-    pgr5 = Programme(p_name='Information Technology Minor')
-    db.session.add(pgr5)
-    db.session.commit()
-    pgr5 = Programme(p_name='Information Technology Minor')
-    db.session.add(pgr5)
-    db.session.commit()
-
-    pgr6 = Programme(p_name='Information Technology Special')
-    db.session.add(pgr6)
-    db.session.commit()
-    pgr6 = Programme(p_name='Information Technology Special')
-    db.session.add(pgr6)
-    db.session.commit()
-
-    print('All programmes added')
-    print('All programmes added')
-
-# This command assigns courses to staff
-@app.cli.command("add-course")
-@click.argument("staff_ID")
-def assign_course(staff_ID):
-    bob = Staff.query.filter_by(u_ID=staff_ID).first()
-
-    if not bob:
-        print(f'Staff with ID: {staff_ID} not found!')
-        return
-      
-    bob.coursesAssigned = ["COMP1601", "COMP1602", "COMP1603"]
-    db.session.add(bob)
-    db.session.commit()
-    print(bob)
-    print('courses added')
-    bob = Staff.query.filter_by(u_ID=staff_ID).first()
-
-    if not bob:
-        print(f'Staff with ID: {staff_ID} not found!')
-        return
-      
-    bob.coursesAssigned = ["COMP1601", "COMP1602", "COMP1603"]
-    db.session.add(bob)
-    db.session.commit()
-    print(bob)
-    print('courses added')
-
-#load course data from csv file
-@app.cli.command("load-courses")
-def load_course_data():
-    with open('courses.csv') as file: #csv files are used for spreadsheets
-      reader = csv.DictReader(file)
-      for row in reader: 
-        new_course = Course(courseCode=row['courseCode'], courseTitle=row['courseTitle'], description=row['description'], 
-          level=row['level'], semester=row['semester'], preReqs=row['preReqs'], p_ID=row['p_ID'],)  #create object
-        db.session.add(new_course) 
-      db.session.commit() #save all changes OUTSIDE the loop
-    print('database intialized')
-
-    with open('courses.csv') as file: #csv files are used for spreadsheets
-      reader = csv.DictReader(file)
-      for row in reader: 
-        new_course = Course(courseCode=row['courseCode'], courseTitle=row['courseTitle'], description=row['description'], 
-          level=row['level'], semester=row['semester'], preReqs=row['preReqs'], p_ID=row['p_ID'],)  #create object
-        db.session.add(new_course) 
-      db.session.commit() #save all changes OUTSIDE the loop
-    print('database intialized')
-"""
