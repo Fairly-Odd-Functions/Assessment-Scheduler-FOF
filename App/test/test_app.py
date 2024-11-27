af24d78b573ab0844ca5bb49663c4b9e2271b9f5
stu-dent101 <katoya.ottley@my.uwi.edu>
2024-11-27 10:08:33 -0400

    08.1.3 User Unit Tests Implementation

--- App/test/test_app.py
+++ App/test/test_app.py
@@ -35,125 +35,22 @@ class UserUnitTest(unittest.TestCase):
 
     #UNIT TEST - #19: Create User
     def test_unit_19_create_user(self):
-        user = create_user("Hope", "Ice", "hopepass", "hope.ice@gmail.com", "staff" )
+        user = User("Hope", "Ice", "hopepass", "hope.ice@gmail.com", "staff" )
         assert user.email == "hope.ice@gmail.com"
 
         #print("User Info:", user) #Testing Output
     
-
-    #UNIT TEST - #20: Get All Admin Users
-    def test_unit_20_get_all_admin_users(self):
-        user = create_user("Jake", "Kim", "jakepass", "jake.kim@gmail.com", "admin")
-        assert user.email == "jake.kim@gmail.com"
-
-        user = create_user("Laim", "Mike", "liampass", "liam.mike@gmail.com", "admin" )
-        assert user.email == "liam.mike@gmail.com"
-
-        admins = get_all_admin_users()    
-
-        #Testing Output
-        '''
-        print("Admin Users: ", admins)
-
-        assert len(admins) == 2
-        assert admins[0].email == "jake.kim@gmail.com"
-        assert admins[1].email == "liam.mike@gmail.com"
-        '''
-
     
-    #UNIT TEST - #21: Get All Staff Users
-    def test_unit_21_get_all_staff_users(self):
-        user = create_user("Nate", "Open", "natepass", "nate.open@gmail.com", "staff")
-        assert user.email == "nate.open@gmail.com"
+    # UNIT TEST - #24: Set Password
+    def test_unit_24_set_password(self):
+        password = "sampass"
+        staff = User("Sam", "Smit", "sampass", "sam.smith@gmail.com", "staff")
+        assert staff.password != password
 
-        user = create_user("Peter", "Quick", "peterpass", "peter.quick@gmail.com", "staff" )
-        assert user.email == "peter.quick@gmail.com"
+        #print("Password: ", staff)
 
-        staff = get_all_staff_users()
-
-        #Testing Output
-        '''
-        print("Admin Users: ", staff)
-
-        assert len(staff) == 2
-        assert staff[0].email == "nate.open@gmail.com"
-        assert staff[1].email == "peter.quick@gmail.com"
-        '''   
-
-    #UNIT TEST - #22: Get All Admin Users JSON
-    def test_unit_22_get_all_admin_users_json(self):
-        user = create_user("Rese", "Tim", "resepass", "rese.tim@gmail.com", "admin")
-        assert user.email == "rese.tim@gmail.com"
-
-        user = create_user("Uma", "Vick", "umapass", "uma.vick@gmail.com", "admin" )
-        assert user.email == "uma.vick@gmail.com"
-
-        admins = get_all_admin_users_json()    
-
-        #Testing Output
-        '''
-        print("Admin Users: ", admins)
-
-        assert len(admins) == 2
-        assert admins[0]['email'] == "rese.tim@gmail.com"
-        assert admins[1]['email'] == "uma.vick@gmail.com"
-        '''
-
-    #UNIT TEST - #23: Get All Staff Users JSON
-    def test_unit_23_get_all_staff_users_json(self):
-        user = create_user("Willow", "Xym", "willowpass", "willow.xym@gmail.com", "staff")
-        assert user.email == "willow.xym@gmail.com"
-
-        user = create_user("Yahem", "Zim", "yahempass", "yahem.zim@gmail.com", "staff" )
-        assert user.email == "yahem.zim@gmail.com"
-
-        staff = get_all_staff_users_json()
-
-        #Testing Output
-        '''
-        print("Admin Users: ", staff)
-
-        assert len(staff) == 2
-        assert staff[0]['email'] == "willow.xym@gmail.com"
-        assert staff[1]['email'] == "yahem.zim@gmail.com"
-        '''  
-
-
-    #UNIT TEST - #24: Validate Staff Users
-    def test_unit_24_validate_Staff(self):
-        user = create_user("Mark", "Sandy", "markpass", "mark.sandy@gmail.com", "staff")
-        assert user.email == "mark.sandy@gmail.com"
-
-        staff = validate_Staff("mark.sandy@gmail.com", "markpass")
-
-        #print("Valid Staff : ", staff) #Testing Output
-
-    
-    #UNIT TEST - #25: Validate Admin Users
-    def test_unit_25_validate_Admin(self):
-        user = create_user("Sammy", "Dan", "sammypass", "sammy.dan@gmail.com", "admin")
-        assert user.email == "sammy.dan@gmail.com"
-
-        staff = validate_Admin("sammy.dan@gmail.com", "sammypass")
-
-        #print("Valid Admin : ", staff) #Testing Output
-
-
-    #UNIT TEST - #26: Get User
-    def test_unit_26_get_user(self):
-        user = create_user("Dean", "Sil", "deanpass", "dean.sil@gmail.com", "admin")
-        assert user.email == "dean.sil@gmail.com"
-
-        getUser = get_user("dean.sil@gmail.com", "deanpass")
-
-        #print("Get Admin : ", getUser) #Testing Output
-
-    
-    #UNIT TEST - #27: Get User ID using their Email
-    def test_unit_27_get_userID(self):
-        user = create_user("Lara", "Silk", "larapass", "“lara.silk@gmail.com", "staff")
-        assert user.email == "“lara.silk@gmail.com"
-
-        getUserID = get_userID("“lara.silk@gmail.com")
-
-        #print("Get User ID : ", getUserID) #Testing Output
+    # UNIT TEST - #25: Check Password
+    def test_unit_25_check_password(self):
+        password = "janepass"
+        staff = User("Jane", "Doe", "janepass", "jane.doeh@gmail.com", "staff")
+        assert staff.check_password(password)
