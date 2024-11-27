a6cd932acf2e8cf3a4398c1c7a8261f85df4eaf2
Rynnia <122883419+RynniaRyan@users.noreply.github.com>
2024-11-26 20:45:10 -0400

    Committing to Rebase Branch [Ignore]
    Every time I create a branch off of main, it is from the old project's main and not ours for some reason. This is work around for that

--- wsgi.py
+++ wsgi.py
@@ -2,19 +2,30 @@ import click, sys, csv
 from flask import Flask
 from App.main import create_app
 from App.database import db, get_migrate
+from App.main import create_app
+from App.database import db, get_migrate
 from flask.cli import with_appcontext, AppGroup
 from App.models import Staff, Course, Assessment, Programme, Admin
 from App.controllers import (
     initialize
+from App.controllers import (
+    initialize
 )
-
+ 
 app = create_app()
 
-# Command 01 : Creates And Initializes The Database
+# This command creates and initializes the database
 @app.cli.command("init", help="Creates And Initializes The Database")
 def init():
     initialize()
-    print('Database Initialized!')
+    print('\n Database Intialized! \n')
+
+'''
+|   Staff Group Commands
+|   These are a list of commands used to perform operations involving existing staff
+'''
+staff_cli = AppGroup('staff', help='Staff object commands')
+
 
 """
 TO BE REFACTORED ~ JaleneA
@@ -22,6 +33,10 @@ TO BE REFACTORED ~ JaleneA
 # This command retrieves all staff objects
 @app.cli.command('get-users')
 def get_users():
+    staff = Staff.query.all()
+    for s in staff:
+      print(s.to_json())
+    print('end of staff objects')
     staff = Staff.query.all()
     for s in staff:
       print(s.to_json())
@@ -34,7 +49,14 @@ def load_Asm():
     asm1 = Assessment(category='EXAM')
     db.session.add(asm1)
     db.session.commit()
+    db.create_all()
+    asm1 = Assessment(category='EXAM')
+    db.session.add(asm1)
+    db.session.commit()
 
+    asm2 = Assessment(category='ASSIGNMENT')
+    db.session.add(asm2)
+    db.session.commit()
     asm2 = Assessment(category='ASSIGNMENT')
     db.session.add(asm2)
     db.session.commit()
@@ -42,7 +64,13 @@ def load_Asm():
     asm3 = Assessment(category='QUIZ')
     db.session.add(asm3)
     db.session.commit()
+    asm3 = Assessment(category='QUIZ')
+    db.session.add(asm3)
+    db.session.commit()
 
+    asm4 = Assessment(category='PROJECT')
+    db.session.add(asm4)
+    db.session.commit()
     asm4 = Assessment(category='PROJECT')
     db.session.add(asm4)
     db.session.commit()
@@ -50,7 +78,13 @@ def load_Asm():
     asm5 = Assessment(category='DEBATE')
     db.session.add(asm5)
     db.session.commit()
+    asm5 = Assessment(category='DEBATE')
+    db.session.add(asm5)
+    db.session.commit()
 
+    asm6 = Assessment(category='PRESENTATION')
+    db.session.add(asm6)
+    db.session.commit()
     asm6 = Assessment(category='PRESENTATION')
     db.session.add(asm6)
     db.session.commit()
@@ -58,7 +92,14 @@ def load_Asm():
     asm7 = Assessment(category='ORALEXAM')
     db.session.add(asm7)
     db.session.commit()
+    asm7 = Assessment(category='ORALEXAM')
+    db.session.add(asm7)
+    db.session.commit()
 
+    asm8 = Assessment(category='PARTICIPATION')
+    db.session.add(asm8)
+    db.session.commit()
+    print('All assessments added')
     asm8 = Assessment(category='PARTICIPATION')
     db.session.add(asm8)
     db.session.commit()
@@ -71,7 +112,14 @@ def load_Pgr():
     pgr1 = Programme(p_name='Computer Science Major')
     db.session.add(pgr1)
     db.session.commit()
+    db.create_all()
+    pgr1 = Programme(p_name='Computer Science Major')
+    db.session.add(pgr1)
+    db.session.commit()
 
+    pgr2 = Programme(p_name='Computer Science Minor')
+    db.session.add(pgr2)
+    db.session.commit()
     pgr2 = Programme(p_name='Computer Science Minor')
     db.session.add(pgr2)
     db.session.commit()
@@ -79,7 +127,13 @@ def load_Pgr():
     pgr3 = Programme(p_name='Computer Science Special')
     db.session.add(pgr3)
     db.session.commit()
+    pgr3 = Programme(p_name='Computer Science Special')
+    db.session.add(pgr3)
+    db.session.commit()
 
+    pgr4 = Programme(p_name='Information Technology Major')
+    db.session.add(pgr4)
+    db.session.commit()
     pgr4 = Programme(p_name='Information Technology Major')
     db.session.add(pgr4)
     db.session.commit()
@@ -87,11 +141,18 @@ def load_Pgr():
     pgr5 = Programme(p_name='Information Technology Minor')
     db.session.add(pgr5)
     db.session.commit()
+    pgr5 = Programme(p_name='Information Technology Minor')
+    db.session.add(pgr5)
+    db.session.commit()
 
     pgr6 = Programme(p_name='Information Technology Special')
     db.session.add(pgr6)
     db.session.commit()
+    pgr6 = Programme(p_name='Information Technology Special')
+    db.session.add(pgr6)
+    db.session.commit()
 
+    print('All programmes added')
     print('All programmes added')
 
 # This command assigns courses to staff
@@ -109,10 +170,30 @@ def assign_course(staff_ID):
     db.session.commit()
     print(bob)
     print('courses added')
+    bob = Staff.query.filter_by(u_ID=staff_ID).first()
+
+    if not bob:
+        print(f'Staff with ID: {staff_ID} not found!')
+        return
+      
+    bob.coursesAssigned = ["COMP1601", "COMP1602", "COMP1603"]
+    db.session.add(bob)
+    db.session.commit()
+    print(bob)
+    print('courses added')
 
 #load course data from csv file
 @app.cli.command("load-courses")
 def load_course_data():
+    with open('courses.csv') as file: #csv files are used for spreadsheets
+      reader = csv.DictReader(file)
+      for row in reader: 
+        new_course = Course(courseCode=row['courseCode'], courseTitle=row['courseTitle'], description=row['description'], 
+          level=row['level'], semester=row['semester'], preReqs=row['preReqs'], p_ID=row['p_ID'],)  #create object
+        db.session.add(new_course) 
+      db.session.commit() #save all changes OUTSIDE the loop
+    print('database intialized')
+"""
     with open('courses.csv') as file: #csv files are used for spreadsheets
       reader = csv.DictReader(file)
       for row in reader: 
