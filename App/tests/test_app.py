a576ad43698cf53f7e2b097038666fb6b405e7ca
DaNJO-Y <daniel.young@my.uwi.edu>
2024-11-28 03:03:14 +0000

    08.2.3 Admin Integration Tests Implementation

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -14,32 +14,13 @@ def empty_db():
     yield app.test_client()
     db.drop_all()
 
-class UserIntegrationTests(unittest.TestCase):
-    
-    # def test_integration_01_authenticate_user_valid(self):
-    #     new_user = create_user("Robert", "Watson", "robpass", "rob22@email.com", "staff")
-    #     response = login_user(new_user)
-    #     assert response is not None
 
-    # def test_integration_02_authenticate_user_invalid(self):
-    #     new_user = create_user()
-
-    def test_integration_02_get_user(self):
-        new_user = create_user("Jenny", "Voe", "voepass", "voe55@email.com", "staff")
-        test = get_user("voe55@email.com","voepass")
-        assert test is not None
-
-    def test_integration_03_get_user_invalid(self):
-        new_user = create_user("Lily", "Daisy", "daisypass", "daisy35@email.com", "staff")
-        test = get_user("daisy35@email.com","notdaisypass")
-        assert test is None
-
-class StaffIntegrationTests(unittest.TestCase):
-    def test_integration_04_create_admin(self):
+class AdminIntegrationTests(unittest.TestCase):
+    def test_integration_05_create_admin(self):
         admin = create_admin("Jill", "Hillington", "jillpass", "jillandhills@email.com")
         assert admin.email == "jillandhills@email.com"
 
-    def test_integration_05_get_all_admin_users_json(self):
+    def test_integration_06_get_all_admin_users_json(self):
         admin = create_admin("David", "Johnson", "davepass", "davesquare@email.com")
         admin_users = get_all_admin_users_json()
         self.assertListEqual([{"firstName" : "Jill",
--- wsgi.py
+++ wsgi.py
@@ -123,16 +123,10 @@ def load_course_data():
 """
 test = AppGroup('test', help='Testing Commands')
 
-@test.command("user", help="Run User Tests")
+@test.command("admin", help="Run User Tests")
 @click.argument("type", default="all")
 def user_tests_command(type):
   if type == "int":
-    sys.exit(pytest.main(["-k", "UserIntegrationTests"]))
-
-@test.command("staff", help="Run User Tests")
-@click.argument("type", default="all")
-def user_tests_command(type):
-  if type == "int":
-    sys.exit(pytest.main(["-k", "StaffIntegrationTests"]))
+    sys.exit(pytest.main(["-k", "AdminIntegrationTests"]))
 
 app.cli.add_command(test)
