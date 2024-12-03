2e8a1f6d59fd7199d65da2f4f6f94dd2ccf796cf
stu-dent101 <katoya.ottley@my.uwi.edu>
2024-12-03 18:27:56 -0400

    10.3.4. Implement API Views for Admin (CourseStaff)

--- App/views/admin.py
+++ App/views/admin.py
@@ -4,7 +4,7 @@ from App.models import Admin
 from datetime import datetime
 from werkzeug.utils import secure_filename
 from flask_jwt_extended import jwt_required
-from App.controllers import Course, CourseAssessment
+from App.controllers import *
 from flask import Blueprint, request, jsonify, render_template, redirect, url_for, flash
 # IMPORTS TO CLEAN UP
 
@@ -170,25 +170,59 @@ def update_offering_action():
 
 """
 CourseStaff [3]
-Written By
+Written ByKatoya Ottley
+Task: 10.3.4. Implement API Views for Admin (CourseStaff)
 """
+# 01 : Assign a Staff to Multiple Courses *
+@admin_views.route('/addStaff', methods=['POST'])
+@jwt_required(Admin)
+def add_courses_to_staff_action():
+    try:
+        data = request.get_json()
+        staffEmail = data.get("staffEmail")
+        courseCodes = data.get("courseCodes")
+        
+        if not staffEmail or not courseCodes :
+            return jsonify(error= "All Fields are Required To Assign Staff to Courses"), 400
+
+        #if not is_valid_staff_id(staffID):
+        #    return jsonify(error = "Invalid Staff ID, Please Try Again."), 400
+
+        newStaff =  add_multiple_courses_to_staff(staffEmail, courseCodes)
+        if newStaff is None:
+            return jsonify(error = "Failed To Add Staff To The Course or Staff Already Assigned to Those Courses."), 400
+
+        message = f'Staff: {newStaff.staffID} Assigned to {courseCodes} Successfully!'
+        return jsonify(message=message), 201
+    
+    except Exception as e:
+        print (f"Error While Assigning Staff To Courses: {e}")
+        return jsonify(error = "An Error Occurred While Assigning Staff To Courses"), 500
+    
+
+''' Present In Admin (Course)
 # 01 : Add Course Staff
 @admin_views.route('/addCourseStaff', methods=['POST'])
 @jwt_required(Admin)
 def add_course_staff_action():
     pass
+'''
 
+'''' Present In Admin (Course)
 # 02 : Remove Course Staff
 @admin_views.route('/removeCourseStaff', methods=['POST'])
 @jwt_required(Admin)
 def remove_course_staff_action():
     pass
+'''
 
+''' Present In Admin (Course)
 # 03 : Update Course Staff
 @admin_views.route('/updateCourseStaff', methods=['POST'])
 @jwt_required(Admin)
 def update_course_staff_action():
     pass
+'''
 
 """
 Programme [3]
